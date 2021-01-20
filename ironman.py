f3e0c8aa683ee9f170582a13c2d518f60d1572a2
Mahesh Mesta <75294271+Mahesh-R-Mesta@users.noreply.github.com>
2021-01-20 18:42:16 +0530

    Update ironman.py

--- ironman.py
+++ ironman.py
@@ -49,6 +49,9 @@ kill_com=False
 get_cor=[]
 Level=1
 Score=0
+text_msg="IRON MAN"
+
+msg=pygame.font.SysFont('comicsan',20)
 label=pygame.font.SysFont("comicsan",35)
 levelsp=label.render("Level:{}".format(Level),1,(255,255,255))
 class velocity():
@@ -106,12 +109,13 @@ class enemy():
     def update_e(self):
         self.win.blit(self.v_img,(self.x,self.y))
 def LifeB(Life,win):
-    b_bar=pygame.Surface((100,10))
-    b_bar.fill((255,255,255))
-    f_bar=pygame.Surface((Life,10))
-    f_bar.fill((0,255,0))
-    win.blit(b_bar,(70,35))
-    win.blit(f_bar,(70,35))
+    if Life>0:
+        b_bar=pygame.Surface((100,10))
+        b_bar.fill((255,255,255))
+        f_bar=pygame.Surface((Life,10))
+        f_bar.fill((0,255,0))
+        win.blit(b_bar,(70,35))
+        win.blit(f_bar,(70,35))
 def collide(obj1,obj2):
     x1=obj1.x - obj2.x
     y1=obj1.y - obj2.y
@@ -130,20 +134,29 @@ def navic(enemyLoc,HeroLoc):
         return   int((HeroLoc-enemyLoc)*((25.0+(11*Level))/(width-80)))
     elif (HeroLoc==enemyLoc):
         return 0
+j=0
+def msg_display(msg,text_msg,win,x,y,j,j1):
+    if len(text_msg)==j:
+        j=len(text_msg)
+        j1+=1
+    else:
+        j+=1
+    msg_dis = msg.render(text_msg[:j],1,(255,255,255))
+    win.blit(msg_dis,(x,y))
+    return j,j1
 
 IRON_MAN = shoot(i_man[1],x_pos,y_pos,win) #Creating My Iron Man
-
-d1=counter(0,15)
+s=0
+s1=0
+d1=counter(0,20)
 d2=counter(0,30)
 enemies=[]
 fps=60
 
 def updateall():
     pygame.display.update()
-surf=pygame.Surface((width,200))
-surf.fill((168,178,179))
-# --------amimation---------- 
 
+# --------suitUp_amimation---------- 
 for suit in first_suit:
     clock.tick(10)
     win.blit(skys,(0,0))
@@ -153,7 +166,6 @@ for suit in first_suit:
         if event.type == pygame.QUIT:
             break
     updateall()
-
 
 #-------------------------------
 while run:
@@ -207,9 +219,9 @@ while run:
         if fire1.x >=width:
             fires.remove(fire1)
     if random.randrange(0, int((fps*2)/Level) ) == 0:
-        enem=enemy(robo1,width+50,random.randrange(robo1.get_height(),height-robo1.get_height()),win)
+        enem=enemy(robo1,width+10,random.randrange(robo1.get_height(),height-robo1.get_height()),win)
         enemies.append(enem)
-    for ene in enemies:
+    for ene in enemies[:]:
         ene.update_e()
         ene.x-=4
         if collide(IRON_MAN,ene):
@@ -221,26 +233,30 @@ while run:
         for fire1 in fires[:]:
             if collide(fire1,ene):
                 enemy_collide(ene)
-                enemies.remove(ene)
                 fires.remove(fire1)
+                enemies.remove(ene)
                 Score+=1
+                break
             for e2 in enemy_2:
                 if collide(fire1,e2):
                     e2.IMG=blast
                     e2.update()
                     enemy_2.remove(e2)
                     fires.remove(fire1)
+                    Score+=5
         if random.randrange(0,fps*2)==0:
             ene_shoot = shoot(missile_fire,ene.x,ene.y+(robo1.get_height()/2),win)
             enemies_fire.append(ene_shoot)
-    if random.randrange(0,900)==0 and main_ene<3 and len(enemy_2)==0:
+    if random.randrange(0,1000/Level)==0 and main_ene<3+(Level-1) and len(enemy_2)==0:
         enemy_2.append(shoot(robo2,width,height-robo2.get_height(),win))
         main_ene+=1
         print("2nd Enemy IS COMMING")
     for ene_2 in enemy_2[:]:
         if ene_2.x > (width-100):
             ene_2.x-=1
+            j1=0
         else:
+            j1,s1=msg_display(msg,"Face My missile Iron Man..",win,ene_2.x-50,ene_2.y-30,j1,0)
             if len(ene2_fire)==0:
                 ene2_fire.append(shoot(robo2_fire,ene_2.x,ene_2.y,win))
         ene_2.update()
@@ -248,7 +264,7 @@ while run:
         fire_2.x-=9
         fire_2.y+=navic(fire_2.y,IRON_MAN.y-30+i_man[1].get_height()/2)
 
-        if IRON_MAN.x-50 > fire_2.x:
+        if IRON_MAN.x-100 > fire_2.x:
             fire_2.IMG=blast
             fire_2.y-=blast.get_height()/2
             fire_2.x-=blast.get_height()/2
@@ -265,28 +281,31 @@ while run:
         fire_2.update()
     win.blit(logo,(10,10))
     kill_com=False
-    
     IRON_MAN.update_myhero(i_man[i],x_pos,y_pos)
     LifeB(Life*10,win)
     levelsp=label.render("Level:{}".format(Level),1,(255,255,255))
     scores=label.render("Score:{}".format(Score),1,(255,255,255))
+
     win.blit(scores,(logo.get_width()+150,15))
-    win.blit(levelsp,(20+logo.get_width(),10) )
+    win.blit(levelsp,(20+logo.get_width(),10))
+    if s<40:
+        j,s=msg_display(msg,text_msg,win,IRON_MAN.x,IRON_MAN.y+30,j,s)
     for ene_s in enemies_fire[:]:
         ene_s.x-=7
         ene_s.update()
         if ene_s.x <=0:
             enemies_fire.remove(ene_s)
         if collide(IRON_MAN,ene_s):
-            ene_s.IMG = small_blast
-            ene_s.x = ene_s.x - small_blast.get_width()/2
-            ene_s.y = ene_s.y - small_blast.get_height()/2
+            ene_s.IMG = blast
+            ene_s.x = ene_s.x - blast.get_width()/2
+            ene_s.y = ene_s.y - blast.get_height()/2
             ene_s.update()
             enemies_fire.remove(ene_s)
             Life-=1
     scr_x+=1
     if scr_x ==(3557+350):
         scr_x = -width
+        text_msg="SUPER SONIC"
         Level+=1
         main_ene=0
     updateall()
